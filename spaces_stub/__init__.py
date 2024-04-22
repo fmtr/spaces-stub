b29ced12ab6d45740c0937aac32ddc95aefcd53e
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-04-22 12:51:49 +0100

    Refine package install process

--- spaces_stub/__init__.py
+++ spaces_stub/__init__.py
@@ -1,3 +1,21 @@
+import subprocess
+import sys
+
+
+def install_package(url):
+    try:
+        # Redirect stdout and stderr to PIPE
+        result = subprocess.run([sys.executable, '-m', 'pip', 'install', url], stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        # Capture stdout and stderr
+        stdout = result.stdout.decode('utf-8')
+        stderr = result.stderr.decode('utf-8')
+        # Return the captured output
+        return stdout, stderr
+    except Exception as e:
+        return None, str(e)
+
+
 def run():
     import importlib
     from os import getenv
@@ -12,7 +30,10 @@ def run():
         raise KeyError('No GITHUB_TOKEN set.')
 
     URL = f'git+https://{GITHUB_TOKEN}@github.com/fmtr/{MODULE_NAME}.git'
-    pip.main(['install', URL])
+
+    stdout, stderr = install_package(URL)
+    print("Stdout:", stdout)
+    print("Stderr:", stderr)
 
     interface = importlib.import_module(f'{MODULE_NAME}.interface')
     interface.run()
