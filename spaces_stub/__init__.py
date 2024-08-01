4407d64ea78831e309dd692082725c3bf305fbfb
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-08-01 12:50:54 +0100

    Use PIP environment variables instead of URL and token

--- spaces_stub/__init__.py
+++ spaces_stub/__init__.py
@@ -1,16 +1,14 @@
-import importlib
-import logging
-import pip
-import subprocess
 import sys
 from os import getenv
-from pathlib import Path
+
+import importlib
+import logging
+import subprocess
 
 
-def install_package(url):
+def install_package(name):
     try:
-        result = subprocess.run([sys.executable, '-m', 'pip', 'install', url], stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
+        result = subprocess.run([sys.executable, '-m', 'pip', 'install', name])
         stdout = result.stdout.decode('utf-8')
         stderr = result.stderr.decode('utf-8')
         return stdout, stderr
@@ -26,23 +24,13 @@ def run():
     if not MODULE_NAME:
         raise KeyError('No MODULE_NAME set.')
 
-    GITHUB_TOKEN = getenv('GITHUB_TOKEN')
-    if not GITHUB_TOKEN:
-        raise KeyError('No GITHUB_TOKEN set.')
-
     print(f'Starting {MODULE_NAME}...')
 
-    URL = f'git+https://{GITHUB_TOKEN}@github.com/fmtr/{MODULE_NAME}.git'
+    subprocess.run([sys.executable, '-m', 'pip', 'install', MODULE_NAME])
 
-    stdout, stderr = install_package(URL)
-
-    GITHUB_TOKEN_MASK = '****'
-    stdout = (stdout or '').replace(GITHUB_TOKEN, GITHUB_TOKEN_MASK)
-    stderr = stderr.replace(GITHUB_TOKEN, GITHUB_TOKEN_MASK)
-
-    logging.debug(f"Pip Output:\n{stdout}")
-    logging.debug(f"Pip Error:\n{stderr}")
-    logging.debug(f"Pip Freeze:\n{Path('/tmp/freeze.txt').read_text()}")
+    # logging.debug(f"Pip Output:\n{stdout}")
+    # logging.debug(f"Pip Error:\n{stderr}")
+    # logging.debug(f"Pip Freeze:\n{Path('/tmp/freeze.txt').read_text()}")
 
     interface = importlib.import_module(f'{MODULE_NAME}.interface')
     interface.run()
