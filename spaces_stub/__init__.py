37cb8fb251d11539cd2537b38395001d7ec086e8
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-08-01 14:28:08 +0100

    Use .netrc file for pip credentials

--- spaces_stub/__init__.py
+++ spaces_stub/__init__.py
@@ -1,32 +1,48 @@
 import sys
-from os import getenv
+from os import getenv, path
 
 import importlib
 import logging
 import subprocess
-
-
-def install_package(name):
-    try:
-        result = subprocess.run([sys.executable, '-m', 'pip', 'install', name, '--no-input'])
-        stdout = result.stdout.decode('utf-8')
-        stderr = result.stderr.decode('utf-8')
-        return stdout, stderr
-    except Exception as e:
-        return None, str(e)
+from pathlib import Path
 
 
 def run():
     FMTR_LOG_LEVEL = getenv('FMTR_LOG_LEVEL', 'INFO')
     logging.getLogger().setLevel(FMTR_LOG_LEVEL)
 
+    home_directory = Path(path.expanduser("~")).absolute()
+
+
+
     MODULE_NAME = getenv('PACKAGE_NAME')
     if not MODULE_NAME:
         raise KeyError('No MODULE_NAME set.')
 
+    PIP_INDEX_URL = getenv('PIP_INDEX_URL')
+    if not PIP_INDEX_URL:
+        raise KeyError('No PIP_INDEX_URL set.')
+
+    PIP_USERNAME = getenv('PIP_USERNAME')
+    if not PIP_USERNAME:
+        raise KeyError('No PIP_USERNAME set.')
+
+    PIP_PASSWORD = getenv('PIP_PASSWORD')
+    if not PIP_PASSWORD:
+        raise KeyError('No PIP_PASSWORD set.')
+
+    lines_nrc = [
+        f'machine {PIP_INDEX_URL}',
+        f'login {PIP_USERNAME}',
+        f'password {PIP_PASSWORD}'
+    ]
+
+    (home_directory / '.netrc').write_text('\n'.join(lines_nrc))
+
     print(f'Starting {MODULE_NAME}...')
 
-    subprocess.run([sys.executable, '-m', 'pip', 'install', MODULE_NAME])
+    subprocess.run(
+        [sys.executable, '-m', 'pip', 'install', MODULE_NAME, '--no-input', f'--index-url http://{PIP_INDEX_URL}'])
 
     # logging.debug(f"Pip Output:\n{stdout}")
     # logging.debug(f"Pip Error:\n{stderr}")
